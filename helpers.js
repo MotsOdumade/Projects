7aaf6579208e65c67291fbb966d367adc7b97ee4
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-05-11 07:19:24 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -252,6 +252,7 @@ async function member_projects_request(targetId){
 try {
     // query the database
     let roleQueryData = await execute_sql_query(roleQuery);
+      console.log("manaher?", roleQueryData);
     if (roleQueryData[0]["Count(*)"] == 0){
       // not a manager
       query2 = query_projects_leading;
